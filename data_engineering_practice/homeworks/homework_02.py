6968a97167ad352edec603331aed33ea3a2e8f7c
Madray Haven <sgpublic2002@gmail.com>
2025-07-06 18:29:36 +0800

    update: update task 14 ~ 15, update homework 02

--- data_engineering_practice/homeworks/homework_02.py
+++ data_engineering_practice/homeworks/homework_02.py
@@ -4,8 +4,14 @@
 要求如下：
 
 1、用 Python 实现 F1-score，并自己构建一个简易数据集进行测试。
+
 2、用 Python 中的 for 循环、列表推导式，分别实现计算：1+2+3+...+1000。
+
 3、随机生成一个五列十行的 Dataframe 的数据类型，行列索引自定义，绘制出对应的柱状图、散点图，以及在查询官网学习绘制一个课程未讲解（即除柱状图、饼图、散点图、箱线图以外的图形）的数据分析图形。
+
+官方文档链接：`Official-Doc`_
+
+.. _Official-Doc: https://pandas.pydata.org/pandas-docs/version/0.23/api.html#api-dataframe-plotting
 """
 import numpy
 import pandas
@@ -60,6 +66,14 @@ def sum_list_comprehension() -> int:
     logger.info(f"结果：{result}")
     return result
 
+def chart_area(base_path: str):
+    data = pandas.DataFrame({'x': numpy.arange(10), 'y': numpy.random.rand(10)})
+    logger.info(f"使用数据：\n{data}\n生成区域图")
+    chart = data.plot.area(x='x', y='y')
+    save_path = f'{base_path}/area.jpg'
+    logger.info(f"保存图片至 {save_path}...")
+    chart.get_figure().savefig(save_path)
+
 def chart_bar(base_path: str):
     data = pandas.DataFrame(numpy.random.rand(10, 4), columns=[x for x in 'abcd'])
     logger.info(f"使用数据：\n{data}\n生成柱状图")
@@ -68,27 +82,61 @@ def chart_bar(base_path: str):
     logger.info(f"保存图片至 {save_path}...")
     chart.get_figure().savefig(save_path)
 
-def chart_scatter(base_path: str):
-    data = pandas.DataFrame(numpy.random.rand(50, 2), columns=['X', 'Y'])
-    logger.info(f"使用数据：\n{data}\n生成散点图")
-    chart = data.plot.scatter(x='X', y='Y')
-    save_path = f'{base_path}/scatter.jpg'
+def chart_barh(base_path: str):
+    data = pandas.DataFrame(numpy.random.rand(10, 4), columns=[x for x in 'abcd'])
+    logger.info(f"使用数据：\n{data}\n生成水平柱状图")
+    chart = data.plot.barh()
+    save_path = f'{base_path}/barh.jpg'
+    logger.info(f"保存图片至 {save_path}...")
+    chart.get_figure().savefig(save_path)
+
+def chart_box(base_path: str):
+    data = pandas.DataFrame(numpy.random.rand(10, 5), columns=[x for x in 'abcde'])
+    logger.info(f"使用数据：\n{data}\n生成箱线图")
+    chart = data.plot.box()
+    save_path = f'{base_path}/box.jpg'
+    logger.info(f"保存图片至 {save_path}...")
+    chart.get_figure().savefig(save_path)
+
+def chart_density_kde(base_path: str):
+    data = pandas.DataFrame(numpy.random.rand(10, 2), columns=['x', 'y'])
+    logger.info(f"使用数据：\n{data}\n生成核密度图")
+    chart = data.plot.density()
+    save_path = f'{base_path}/density.jpg'
     logger.info(f"保存图片至 {save_path}...")
     chart.get_figure().savefig(save_path)
 
 def chart_hexbin(base_path: str):
-    """
-    官方文档链接：`Official-Doc`_
-    此处绘制一个六边形分箱图。
-
-    .. _Official-Doc: https://pandas.pydata.org/pandas-docs/version/0.23/api.html#api-dataframe-plotting
-    """
     data = pandas.DataFrame(numpy.random.rand(1000, 2), columns=['x', 'y'])
     logger.info(f"使用数据：\n{data}\n六边形分箱图")
     chart = data.plot.hexbin(x='x', y='y', gridsize=20)
     save_path = f'{base_path}/hexbin.jpg'
     chart.get_figure().savefig(save_path)
     logger.info(f"保存图片至 {save_path}")
+
+def chart_hist(base_path: str):
+    data = pandas.DataFrame(numpy.random.rand(10, 2), columns=['a', 'b'])
+    logger.info(f"使用数据：\n{data}\n生成直方图")
+    chart = data.plot.hist()
+    save_path = f'{base_path}/hist.jpg'
+    logger.info(f"保存图片至 {save_path}...")
+    chart.get_figure().savefig(save_path)
+
+def chart_line(base_path: str):
+    data = pandas.DataFrame({'x': numpy.arange(20), 'y': numpy.random.rand(20)})
+    logger.info(f"使用数据：\n{data}\n生成折线图")
+    chart = data.plot.line(x='x', y='y')
+    save_path = f'{base_path}/line.jpg'
+    logger.info(f"保存图片至 {save_path}...")
+    chart.get_figure().savefig(save_path)
+
+def chart_scatter(base_path: str):
+    data = pandas.DataFrame(numpy.random.rand(50, 2), columns=['x', 'y'])
+    logger.info(f"使用数据：\n{data}\n生成散点图")
+    chart = data.plot.scatter(x='x', y='y')
+    save_path = f'{base_path}/scatter.jpg'
+    logger.info(f"保存图片至 {save_path}...")
+    chart.get_figure().savefig(save_path)
 
 if __name__ == '__main__':
     logger.debug("--- F1-score ---")
@@ -107,7 +155,13 @@ if __name__ == '__main__':
     base_path = './outputs/homework-02'
     logger.info(f"创建文件夹 {base_path} 以存放输出图片...")
     os.makedirs(base_path, exist_ok=True)
+    chart_area(base_path)
     chart_bar(base_path)
-    chart_scatter(base_path)
+    chart_barh(base_path)
+    chart_box(base_path)
+    chart_density_kde(base_path)
     chart_hexbin(base_path)
+    chart_hist(base_path)
+    chart_line(base_path)
+    chart_scatter(base_path)
     logger.debug("--- chart bar ---")
---
+++ data_engineering_practice/tasks/task_15.py
@@ -0,0 +1,16 @@
+"""
+# 2.2.t15 实验任务 15
+
+指导书要求编写代码使用 Pandas 中的 Plot 类，用于数据可视化分析。
+"""
+import numpy
+import pandas
+import os
+
+from data_engineering_practice.utils.logging import new_logger
+
+
+logger = new_logger("task-14")
+
+if __name__ == '__main__':
+    pass
